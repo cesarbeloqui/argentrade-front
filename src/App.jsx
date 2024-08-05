11202a0b46f2718faaedb3e70362cfa29545496c
cesarbeloqui <beloqui.cesar@gmail.com>
2024-08-05 05:47:36 -0300

    primeros ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <h1>Hola mundo</h1>
-  )
+  return <Header></Header>;
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      primary: "#F2F2F2",
+      secondary: "#03006D",
+      azul: "#1B1464",
+      naranja: "#F29100",
+      white: "#FFFFFF",
+      black: "#000000",
+      gray: {
+        100: "#F2F2F2",
+        200: "#E5E5E5",
+        300: "#D9D9D9",
+        400: "#CCCCCC",
+        500: "#BFBFBF",
+        600: "#B3B3B3",
+        700: "#A6A6A6",
+        800: "#999999",
+        900: "#8C8C8C",
+      },
+    },
+    fontFamily: {
+      sans: ["Raleway", "sans-serif"],
+      serif: ["Roboto", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [forms],
+};
