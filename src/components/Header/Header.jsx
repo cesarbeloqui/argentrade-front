6c6311f5668b563b376697119002a24c37aaf871
cesarbeloqui <beloqui.cesar@gmail.com>
2024-08-05 07:11:22 -0300

    navbar terminada

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,12 +24,12 @@ export default function Example() {
               <div className="flex w-full justify-between">
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="block h-8 w-auto lg:hidden"
+                    className="block h-10 w-auto lg:hidden"
                     src={Logo}
                     alt="Argentrade"
                   />
                   <img
-                    className="hidden h-8 w-auto lg:block"
+                    className="hidden h-10 w-auto lg:block"
                     src={Logo}
                     alt="Argentrade"
                   />
@@ -42,9 +42,9 @@ export default function Example() {
                       href={item.href}
                       className={classNames(
                         item.current
-                          ? "border-indigo-500 text-gray-900"
-                          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                        "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
+                          ? "text-naranja font-extrabold"
+                          : "text-azul hover:text-naranja font-bold",
+                        "inline-flex items-center px-1 pt-1 text-sm font-medium"
                       )}
                       aria-current={item.current ? "page" : undefined}
                     >
