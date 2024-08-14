3f9e11b57afafed8a890bd9a9a349457f85a5f56
cesarbeloqui <beloqui.cesar@gmail.com>
2024-08-14 14:27:15 -0300

    front terminado

--- src/components/ParaQuienes/SvgCirculo.jsx
+++ src/components/ParaQuienes/SvgCirculo.jsx
@@ -3,7 +3,6 @@ import useLocalizedContent from "../../hooks/useLocalizedContent";
 const LogoComponent = ({ className, id, elementsRef }) => {
 	const { paraQuienesSection } = useLocalizedContent();
 	const { circulo } = paraQuienesSection;
-	console.log(paraQuienesSection);
   return (
     <div
       dangerouslySetInnerHTML={{ __html: circulo }}
