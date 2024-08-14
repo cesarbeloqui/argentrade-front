c3e8d7587e3e75ef61b9070c95b78e43a58dc0ad
cesarbeloqui <beloqui.cesar@gmail.com>
2024-08-14 14:43:30 -0300

    ajustes

--- src/components/ChinaSection/ChinaSection.jsx
+++ src/components/ChinaSection/ChinaSection.jsx
@@ -4,15 +4,15 @@ import ReasaltarTexto from "../ReasaltarTexto/ResaltarTexto";
 const ChinaSection = () => {
   const { chinaSection } = useLocalizedContent();
   return (
-    <div className="relative h-[65vh] sm:h-[30vw] sm:w-full bg-china-section bg-no-repeat bg-center bg-cover flex justify-end items-middle p-14 sm:p-0">
-      <div className="sm:w-[49%] sm:pt-[4vw] sm:pr-[4vw]">
+    <div className="relative sm:h-[30vw] sm:w-full bg-china-section bg-no-repeat sm:bg-center bg-cover flex justify-end items-center p-14 sm:p-0">
+      <div className="flex flex-col sm:w-[49%] sm:pr-[4vw]">
         <h1 className="sm:text-[2.2vw] font-extrabold text-stroke-1-white text-left">
           {chinaSection.lead}
         </h1>
         <h1 className="sm:text-[3.55vw] text-white font-extrabold text-left sm:-mt-2">
           {chinaSection.titulo}
         </h1>
-        <h2 className="sm:text-[1.8vw] text-white text-left sm:mt-[4.5vw] font-medium">
+        <h2 className="sm:text-[1.8vw] text-white text-left sm:mt-[2vw] font-medium">
           <ReasaltarTexto text={chinaSection.contenido} className="font-bold" />
         </h2>
       </div>
