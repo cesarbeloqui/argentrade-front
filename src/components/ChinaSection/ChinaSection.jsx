19a913123a64211274743214baa96d2912252252
cesarbeloqui <beloqui.cesar@gmail.com>
2024-08-15 17:14:53 -0300

    terminado

--- src/components/ChinaSection/ChinaSection.jsx
+++ src/components/ChinaSection/ChinaSection.jsx
@@ -5,7 +5,7 @@ const ChinaSection = () => {
   const { chinaSection } = useLocalizedContent();
   return (
     <div className="relative h-[40vh] sm:h-[30vw] sm:w-full bg-china-section bg-no-repeat bg-[-2rem] sm:bg-center bg-cover flex justify-end items-center p-14 sm:p-0">
-      <div className="flex flex-col pt-[20vh] sm:w-[49%] sm:pr-[4vw]">
+      <div className="flex flex-col pt-[20vh] sm:pt-0 sm:w-[49%] sm:pr-[4vw]">
         <h1 className="sm:text-[2.2vw] font-extrabold text-stroke-1-white text-left">
           {chinaSection.lead}
         </h1>
