9088c2d51b05ec86272fd6966c292f96df50f3e2
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-11 14:27:42 -0300

    gvgdfg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,10 @@ import useLocalizedContent from "../../hooks/useLocalizedContent";
 const Footer = () => {
   const { footer } = useLocalizedContent();
   const { lista1, redes } = footer;
+  const fechaActual = new Date();
+  const añoActual = fechaActual.getFullYear();
   return (
-    <section className="container-footer w-full bg-secondary py-[4vh]">
+    <><section className="container-footer w-full bg-secondary py-[4vh]">
       <div></div>
       <div className="logo">
         <a href="/#">
@@ -30,8 +32,7 @@ const Footer = () => {
                 <img
                   src={item.icon}
                   className="w-[9vw] sm:w-[2.5vw] mr-4"
-                  alt={item.titulo}
-                />
+                  alt={item.titulo} />
                 <p className="text-white font-semibold sm:text-base text-[3.4vw]">
                   {item.titulo}
                 </p>
@@ -50,14 +51,27 @@ const Footer = () => {
               <img
                 src={item.icon}
                 className="sm:w-[2.3vw] w-[8.5vw]"
-                key={index}
-              />
+                key={index} />
             </a>
           ))}
         </div>
       </div>
       <div></div>
-    </section>
+    </section><div className='w-full bg-secondary text-xs sm:text-sm py-2 px-4 sm:px-12 flex flex-col sm:flex-row justify-between items-center border-t-0 border-secondary text-white'>
+        <div className='mb-2 sm:mb-0 text-center'>
+          {`© ${añoActual} Argetrade. Tu socio en el mundo`}<br className='sm:hidden' /> <span className='hidden sm:inline'>|</span> Todos los derechos reservados
+        </div>
+        <div className='mt-2 sm:mt-0'>
+          Diseño web:{' '}
+          <a
+            href='https://www.reddisegno.com.ar/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-naranja'>
+            REDdiseño
+          </a>
+        </div>
+      </div></>
   );
 };
 export default Footer;
